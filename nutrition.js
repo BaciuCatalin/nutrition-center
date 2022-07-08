69fe6b37e9d3f61ef702e33d04bbda4971f09dc9
BaciuCatalin <catalin.baciu@yahoo.com>
2022-07-08 10:35:42 +0300

    renamed variables

--- nutrition.js
+++ nutrition.js
@@ -1,30 +1,30 @@
-console.log("main.js");
+console.log("nutrition.js");
 
 let firstName = document.querySelector("#firstName");
 let secondName = document.querySelector("#secondName");
 let height = document.querySelector("#height");
-let weight = document.querySelector("#weight");
+let weight = document.querySelector("#weight"); 
+let gender = document.getElementsByName("gender")[0];
 
 let genderType;
 function result() {
-  let gender = document.getElementsByName("gender")[0];
   genderType = gender.options[gender.selectedIndex].text;
   testedValue();
 }
 
 
-let newHeight = 0;
-let newWeight = 0;
+let testedHeight = 0;
+let testeWeight = 0;
 function testedValue() {
   let errors = false;
 
-  newHeight = testUserValue(height, "Please input a real value for Height!");
-  if (newHeight == false) {
+  testedHeight = testUserValue(height, "Please input a real value for Height!");
+  if (testedHeight == false) {
     errors = true;
   }
 
-  newWeight = testUserValue(weight, "Please input a real value for Weight!");
-  if (newWeight == false) {
+  testeWeight = testUserValue(weight, "Please input a real value for Weight!");
+  if (testeWeight == false) {
     errors = true;
   }
 
@@ -32,17 +32,18 @@ function testedValue() {
     return;
   } 
   else {
-    if (newHeight == parseInt(newHeight)) {
-      newHeight = newHeight / 100;
+    if (testedHeight == parseInt(testedHeight)) {
+      testedHeight = testedHeight / 100;
     }
     calculate();
   }
 }
 
+
 let BMI;
 let range;
 function calculate() {
-  BMI = Math.round(newWeight / (newHeight * newHeight));
+  BMI = Math.round(testeWeight / (testedHeight * testedHeight));
   if (BMI < 18.5) {
     range = "underweight";
   } else if (BMI >= 18.5 && BMI < 25) {
@@ -56,6 +57,7 @@ function calculate() {
   let display = document.querySelector(".display");
   display.innerText = firstName.value + " " + secondName.value + " | " + genderType + " | BMI:" + BMI + " | " + range;
 }
+
 
 function testUserValue(textbox, errorMsg) {
   textbox.style.borderColor = "black";
