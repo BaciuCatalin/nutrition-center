a9152f50ab491ab22bd1cd576646a43cbabe6830
BaciuCatalin <catalin.baciu@yahoo.com>
2023-03-22 22:08:44 +0200

    ref

--- nutrition.js
+++ nutrition.js
@@ -1,10 +1,11 @@
 console.log("nutrition.js");
 
-let firstName = document.querySelector("#firstName");
-let secondName = document.querySelector("#secondName");
-let height = document.querySelector("#height");
-let weight = document.querySelector("#weight"); 
-let gender = document.getElementsByName("gender")[0];
+let firstName = document.querySelector('#firstName');
+let secondName = document.querySelector('#secondName');
+let gender = document.getElementsByName('gender')[0];
+
+let height = document.querySelector('#height');
+let weight = document.querySelector('#weight');
 
 let genderType;
 function result() {
@@ -12,9 +13,8 @@ function result() {
   testedValue();
 }
 
-
-let testedHeight = 0;
-let testeWeight = 0;
+let testedHeight;
+let testedWeight;
 function testedValue() {
   let errors = false;
 
@@ -23,41 +23,63 @@ function testedValue() {
     errors = true;
   }
 
-  testeWeight = testUserValue(weight, "Please input a real value for Weight!");
-  if (testeWeight == false) {
+  testedWeight = testUserValue(weight, "Please input a real value for Weight!");
+  if (testedWeight == false) {
     errors = true;
   }
 
   if (errors) {
     return;
-  } 
-  else {
+  } else {
     if (testedHeight == parseInt(testedHeight)) {
       testedHeight = testedHeight / 100;
     }
-    calculate();
+    displayA();
   }
 }
 
+const BMIrange = {
+  UNDERWEIGHT: "Underweight",
+  NORMAL: "Normal",
+  OVERWEIGHT: "Overweight",
+  OBESE: "Obese",
+};
 
-let BMI;
+let BMI = 0;
 let range;
-function calculate() {
-  BMI = Math.round(testeWeight / (testedHeight * testedHeight));
-  if (BMI < 18.5) {
-    range = "underweight";
-  } else if (BMI >= 18.5 && BMI < 25) {
-    range = "normal";
-  } else if (BMI >= 25 && BMI < 30) {
-    range = "overweight";
-  } else {
-    range = "obese";
-  }
+function BMIcalcul() {
+  BMI = (testedWeight / testedHeight ** 2).toFixed(0)
+  
+  const rangeResult = (BMI) => {
+    if (BMI < 18.5) {
+      return BMIrange.UNDERWEIGHT;
+    }
+    if (BMI < 25) {
+      return BMIrange.NORMAL;
+    }
+    if (BMI < 30) {
+      return BMIrange.OVERWEIGHT;
+    }
+    return BMIrange.OBESE;
+  };
 
-  let display = document.querySelector(".display");
-  display.innerText = firstName.value + " " + secondName.value + " | " + genderType + " | BMI:" + BMI + " | " + range;
+  range = rangeResult(BMI)
 }
 
+function displayA() {
+  BMIcalcul()
+  let display = document.querySelector(".display");
+  display.innerText =
+    firstName.value +
+    " " +
+    secondName.value +
+    " | " +
+    genderType +
+    " | BMI:" +
+    BMI +
+    " | " +
+    range;
+}
 
 function testUserValue(textbox, errorMsg) {
   textbox.style.borderColor = "black";
